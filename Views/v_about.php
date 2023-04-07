2adecbaece882afaf589953e56662b0f4235f601
Brave345 <rajuabdullah23@gmail.com>
2023-04-07 20:06:48 +0700

    Tugas Pertemuan ke 3

---
+++ Views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+<h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Raju </li>
+ <li>Nama Belakang : Abdullah</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Bekasi Timur MGT</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>PekanBaru</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Karate</li>
+ <li>GYm</li>
+ </ul>
+</section>
